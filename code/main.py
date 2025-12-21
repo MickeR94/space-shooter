25cdf6ede9d55923b4d9517352e4ebbab77a2d20
MickeR94 <mikael.rudenvald@hotmail.se>
2025-12-21 23:36:45 +0100

    Adding a surface to the display. In this case, a rectangle in the middle of the screen

--- code/main.py
+++ code/main.py
@@ -8,6 +8,8 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
 
+# surface - a rectangle is made
+surf = pygame.Surface((100, 200))
 
 while running:
     # Event loop
@@ -17,10 +19,11 @@ while running:
     
     # Draw the game
     display_surface.fill("blue")
+    display_surface.blit(surf, (615,250))
     pygame.display.update() #update updates the entire window
     
 
 # Closing the game properly to avoid weird behaviour   
 pygame.quit()
 
-# Stannar 16:04 https://www.youtube.com/watch?v=8OMghdHP-zs
+# Stannar 20:57 https://www.youtube.com/watch?v=8OMghdHP-zs
