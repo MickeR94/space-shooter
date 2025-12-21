9a09b9ec8299bdd45d4d4f691032b3c54b129331
MickeR94 <mikael.rudenvald@hotmail.se>
2025-12-21 23:57:18 +0100

    Added the spaceship, moving constantly to the right

--- code/main.py
+++ code/main.py
@@ -1,15 +1,20 @@
 import pygame
-
+from os.path import join
 
 #general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT  = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Space Shooter")
+pygame.display.set_caption('Space Shooter')
 running = True
 
-# surface - a rectangle is made
+# plain surface
 surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+# import an image
+player_surf = pygame.image.load(join('images', 'player.png'))
 
 while running:
     # Event loop
@@ -18,12 +23,14 @@ while running:
             running = False
     
     # Draw the game
-    display_surface.fill("blue")
-    display_surface.blit(surf, (615,250))
+    display_surface.fill('darkgray')
+    x += 0.1
+    # the ship is positioned at (x, 250)
+    display_surface.blit(player_surf, (x,250))
     pygame.display.update() #update updates the entire window
     
 
 # Closing the game properly to avoid weird behaviour   
 pygame.quit()
 
-# Stannar 20:57 https://www.youtube.com/watch?v=8OMghdHP-zs
+# Stannar 36:05 https://www.youtube.com/watch?v=8OMghdHP-zs
