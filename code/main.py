ffa79e108f1f937efc81d6630dbb23112087c695
MickeR94 <mikael.rudenvald@hotmail.se>
2025-12-22 10:28:47 +0100

    Added stars at random positions in the background

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pygame
 from os.path import join
+from random import randint
 
 #general setup
 pygame.init()
@@ -13,8 +14,10 @@ surf = pygame.Surface((100, 200))
 surf.fill('orange')
 x = 100
 
-# import an image
-player_surf = pygame.image.load(join('images', 'player.png'))
+# import an image, the space ship and stars. Use convert/convert_alpha for improved performance
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_positions = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
     # Event loop
@@ -27,10 +30,13 @@ while running:
     x += 0.1
     # the ship is positioned at (x, 250)
     display_surface.blit(player_surf, (x,250))
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
     pygame.display.update() #update updates the entire window
+    
     
 
 # Closing the game properly to avoid weird behaviour   
 pygame.quit()
 
-# Stannar 36:05 https://www.youtube.com/watch?v=8OMghdHP-zs
+# Stannar 42:15 https://www.youtube.com/watch?v=8OMghdHP-zs
